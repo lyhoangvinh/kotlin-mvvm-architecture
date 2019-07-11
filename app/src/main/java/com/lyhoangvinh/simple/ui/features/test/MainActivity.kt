64edb28306262966e3312b8310385675f6e7e926
Lý Hoàng Vinh <vinh270795@gmail.com>
2019-07-11 17:31:44 +0700

    no message

--- app/src/main/java/com/lyhoangvinh/simple/ui/features/test/MainActivity.kt
+++ app/src/main/java/com/lyhoangvinh/simple/ui/features/test/MainActivity.kt
@@ -1,17 +1,7 @@
 package com.lyhoangvinh.simple.ui.features.test
 
-import android.os.Bundle
-import com.lyhoangvinh.simple.R
 import com.lyhoangvinh.simple.data.entinies.comic.Issues
-import com.lyhoangvinh.simple.databinding.ActivityMainBinding
+import com.lyhoangvinh.simple.databinding.ViewRecyclerviewBinding
 import com.lyhoangvinh.simple.ui.base.activity.BaseViewModelRecyclerViewActivity
 
-class MainActivity : BaseViewModelRecyclerViewActivity<ActivityMainBinding, MainViewModel, MainAdapter, Issues>() {
-
-    override fun getLayoutResource() = R.layout.activity_main
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding.vm = viewModel
-    }
-}
+class MainActivity : BaseViewModelRecyclerViewActivity<ViewRecyclerviewBinding, MainViewModel, MainAdapter, Issues>()
